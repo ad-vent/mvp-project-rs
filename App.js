bede4af3d406ca553fb28f6f9b3056d0c76edc96
Adrian <vent.adrian@gmail.com>
2020-07-25 10:29:40 -0700

    app cleanup

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import 'react-native-gesture-handler';
-import React, { useState } from 'react';
+import React from 'react';
 
 import Navigator from './routes/drawer';
-// import Welcome from './src/components/Welcome';
 
 export default function App() {
   return (
