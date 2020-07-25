8b6f830a11e51a87a71af2582f8fab8cadf29448
Adrian <vent.adrian@gmail.com>
2020-07-25 10:27:44 -0700

    first commit

--- server/controller.js
+++ server/controller.js
@@ -1,1 +1,14 @@
 const Recipe = require('../database/recipe');
+
+const getUserInfo = (req, res) => {
+  const user = req.param.username
+  Recipe.find({ username: user })
+    .exec((err, data) => {
+      if (err) res.sendStatus(400);
+      res.send(data);
+    });
+}
+
+module.exports = {
+  getUserInfo
+}
--- shared/parser.js
+++ shared/parser.js
@@ -1,1 +1,16 @@
 import axios from ('axios');
+
+const getUserInfo = (username, callback) => {
+  axios({
+    method: 'GET',
+    url: `/api/${username}`
+  }).then((res) => {
+    callback(res.data);
+  }).catch((err) => {
+    callback(err)
+  });
+};
+
+export default {
+  getUserInfo
+}
